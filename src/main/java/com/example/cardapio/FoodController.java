f45485315b29eef957d39842b9b33cf259f2238f
Azevedo <beyondtracking@gmail.com>
2024-07-30 21:03:41 -0300

    Initial commit

---
+++ src/main/java/com/example/cardapio/FoodController.java
@@ -0,0 +1,24 @@
+package com.example.cardapio;
+
+import food.Food;
+import food.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+
+    @Autowired
+    private FoodRepository repository;
+    @GetMapping
+    public List<Food> getAll(){
+
+        List<Food> foodList = repository.findAll();
+        return foodList;
+
+    }
+}
